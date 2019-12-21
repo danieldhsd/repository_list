cf0bf727726ece45991da9a1f7a360e830a3b0d1
Daniel Dutra <danieldhsd@gmail.com>
2019-12-21 13:06:35 -0300

    Navegacao de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Main from "./pages/Main";
-import Repository from "./pages/Repository";
+import Main from './pages/Main';
+import Repository from './pages/Repository';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main}></Route>
-        <Route path="/repository" exact component={Repository}></Route>
+        <Route path="/repository/:repository" component={Repository}></Route>
       </Switch>
     </BrowserRouter>
   );
